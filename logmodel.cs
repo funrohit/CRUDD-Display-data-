45501dafae1795028c248c0166744a81f60f6914
ROHIT SINGH <36737110+funrohit@users.noreply.github.com>
2022-06-09 17:11:40 +0530

    Models

---
+++ logmodel.cs
@@ -0,0 +1,13 @@
+﻿namespace SMS.Models
+{
+    public class logmodel
+    {
+        public int Id { get; set; }
+
+        public int? AdminId { get; set; }
+
+        public string? Pass { get; set; }
+
+
+    }
+}
---
+++ mymodel.cs
@@ -0,0 +1,46 @@
+﻿namespace SMS.Models
+{
+    public class mymodel
+    {
+
+        public int Id { get; set; }
+
+        public string? Sname { get; set; }
+
+        public int? Roll { get; set; }
+
+        public int? Class { get; set; }
+
+        public int? Age { get; set; }
+
+        public string? Section { get; set; }
+
+        public string? ClassTeacher { get; set; }
+
+        public string? Principal { get; set; }
+
+        public string? BloodGroup { get; set; }
+
+        public string? HouseDress { get; set; }
+
+        public string? Details { get; set; }
+
+        public string? Performance { get; set; }
+
+        public string? Doa { get; set; }
+
+        public string? Dob { get; set; }
+
+        public string? CurrentFee { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? AdharNo { get; set; }
+
+
+    }
+}
